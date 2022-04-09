7f54c8b7143bab760b148ffb9087f2578582f8e9
mrdrzit <uzunaruto.10.mat@gmail.com>
2022-04-09 12:52:32 -0300

    Delete folder

--- draft/Plot_EPM.py
+++
@@ -1,2 +0,0 @@
-def Plot_EPM():
-  print("Plotting figures...")
--- draft/Video_Analyse.py
+++
@@ -1,2 +0,0 @@
-def Video_Analyse(Input_Settings,File_Header,Data):
-  print("Using Input_Settings, File_Header and Data to analyse files...")
