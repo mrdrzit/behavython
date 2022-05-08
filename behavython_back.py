09d06a66f7ed667738e887f64895e755ff60dacf
mrdrzit <uzunaruto.10.mat@gmail.com>
2022-05-07 21:42:37 -0300

    Add explaining comment

--- behavython_back.py
+++ behavython_back.py
@@ -82,7 +82,7 @@ class experiment_class:
 
         # TODO create check to verify that the csv file have the correct number of data columns  
         quadrant_data = np.array(self.data[[2,3,4,5,6]])                                           # Extract the quadrant data from csv file
-        colDif = np.abs(quadrant_data[:,0] - np.sum(quadrant_data[:][0:],axis=1))                  # ver porque no valor 205 (animal 2) já tem um "2" (off by one?)
+        colDif = np.abs(quadrant_data[:,0] - np.sum(quadrant_data[:][0:],axis=1))                  # Here, the values will be off-by-one because MATLAB starts at 1
         full_entry_indexes = colDif == 1                                                           # Create a logical array where there is "full entry"
         full_entry_indexes = np.int32(full_entry_indexes)                                          # Convert from logical to int
         c = np.array([(1-x) for x in full_entry_indexes])
