9d5cca3e7b7dfdfcf0435e20474d30d862df9bed
mrdrzit <uzunaruto.10.mat@gmail.com>
2022-06-26 11:23:12 -0300

    Better path handling

--- Video_analyse_validation/analyse_test.py
+++ Video_analyse_validation/analyse_test.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
+import os
 
 # import animal_2_MATLAB.xlsx and animal_2_PYTHON.xlsx then compare the second column of each file against each other
 # if they are the same, then print out "TEST PASSED" else if they are not the same, print out "TEST FAILED" 
 
-
-matlab = pd.read_excel("Behavython/Video_analyse_validation/animal_2_MATLAB.xlsx", header=None)
-python = pd.read_excel("Behavython/Video_analyse_validation/animal_2_PYTHON.xlsx", header=None)
+path_python = os.path.dirname(__file__) + '\\animal_2_PYTHON.xlsx'
+path_matlab = os.path.dirname(__file__) + '\\animal_2_MATLAB.xlsx'
+matlab = pd.read_excel(path_python, header=None)
+python = pd.read_excel(path_matlab, header=None)
 
 def test_analyse():
   for i in range(0, len(matlab[1])): 
