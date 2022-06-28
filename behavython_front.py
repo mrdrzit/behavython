77ce0b71d8679b554db143577ee261c3ec0b3c5d
João Pedro <mcjpedro@gmail.com>
2022-06-28 19:47:37 -0300

    Update

--- behavython_front.py
+++ behavython_front.py
@@ -1,9 +1,10 @@
-from Widgets.custom_widgets import plot_viewer_function
 import sys
 import os
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtCore import QObject, QThread, pyqtSignal
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 import os
 import skimage.io
@@ -60,8 +61,7 @@ class behavython_gui(QMainWindow):
 
         super(behavython_gui, self).__init__()              # Calls the inherited classes __init__ method
         load_gui_path = os.path.dirname(__file__) + "\\Behavython_GUI.ui"
-        #sys.path.append("./QtCustomWidgets/widgets")
-        uic.loadUi(load_gui_path, self, package='Widgets')               # Loads the interface design archive (made in Qt Designer)
+        uic.loadUi(load_gui_path, self, package='behavython_front')               # Loads the interface design archive (made in Qt Designer)
         self.show()
 
         self.options = {}
@@ -111,6 +111,34 @@ class behavython_gui(QMainWindow):
         self.arena_height_lineedit.setText('65')
         self.animal_combobox.setCurrentIndex(0)
         plot_viewer_function()
+
+class plot_viewer_function(QtWidgets.QWidget):
+    '''
+    This class modifies the interface's QWidget in order to insert a plot viewer.
+    
+    TODO: Find the best way to plot a summary of the data.
+    E.g.: A button to advance or go back in the plots
+    '''    
+    def __init__(self, parent = None):
+        QtWidgets.QWidget.__init__(self, parent)                                   
+        self.canvas = FigureCanvas(Figure(facecolor = "#353535", dpi=100, tight_layout=True))          # Create a figure object 
+        
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding, 
+                                           QtWidgets.QSizePolicy.Policy.Expanding)  # Creates axes size policy
+        self.canvas.setSizePolicy(sizePolicy)                                       # Sets the size policy
+        
+        self.canvas.axes = []
+        for i in range(1,10):
+            self.canvas.axes.append(self.canvas.figure.add_subplot(3,3,i))          # Creates an empty plot
+            self.canvas.axes[i-1].set_facecolor("#252525")                          # Changes the plot face color
+            self.canvas.axes[i-1].get_xaxis().set_visible(False)
+            self.canvas.axes[i-1].get_yaxis().set_visible(False)
+        
+        vertical_layout = QtWidgets.QVBoxLayout()                                   # Creates a layout
+        vertical_layout.addWidget(self.canvas)                                      # Inserts the figure on the layout
+        self.canvas.figure.subplots_adjust(left=0, bottom=0, right=1, 
+                                           top=1, wspace=0, hspace=0)               # Sets the plot margins 
+        self.setLayout(vertical_layout)                                             # Sets the layout
 
 class experiment_class:
     '''
