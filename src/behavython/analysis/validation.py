8d7e99805df477b96e30e8e7d7bd5c7e221c2304
Matheus C. <uzunaruto.10.mat@gmail.com>
2026-03-29 21:42:10 -0300

    Add defaults to prepare for analysis port

--- src/behavython/analysis/validation.py
+++ src/behavython/analysis/validation.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import os
-
 from behavython.analysis.models import analysis_request
 
 
--- src/behavython/config/defaults.py
+++ src/behavython/config/defaults.py
@@ -22,3 +22,46 @@ ANALYSIS_REQUIRED_SUFFIXES = {
 VALID_VIDEO_EXTENSIONS = (".mp4", ".avi", ".mov")
 
 TOTAL_SESSION_STORAGE_QUOTA = 5
+
+CANONICAL_BODYPARTS = [
+    "nose",
+    "left_ear",
+    "right_ear",
+    "center",
+    "tail",
+]
+
+CANONICAL_REQUIRED_BODYPARTS = [
+    "nose",
+    "left_ear",
+    "right_ear",
+    "center",
+]
+
+BODY_PART_MAPPING = {
+    "nose": "focinho",
+    "left_ear": "orelhae",
+    "right_ear": "orelhad",
+    "center": "centro",
+}
+
+EXPERIMENT_TYPES = [
+    "social_recognition",
+    "social_discrimination",
+    "object_discrimination",
+]
+
+ROI_COUNT_BY_EXPERIMENT = {
+    "social_recognition": 1,
+    "social_discrimination": 2,
+    "object_discrimination": 2,
+}
+
+DEFAULT_ANALYSIS_PARAMETERS = {
+    "frames_per_second": 30,
+    "arena_width": 30,
+    "arena_height": 30,
+    "task_duration": 300,
+    "trim_amount": 0,
+    "threshold": 5,
+}
